a77392991ea9ce73752dddf524f70bdb94bc5fcc
JorVa <jorvacoding@outlook.com>
2016-10-04 21:31:12 -0400

    Bug Fix & Batteries :D (they work)

--- src/main/java/engineers/core/items/batteries/ItemBattery.java
+++ src/main/java/engineers/core/items/batteries/ItemBattery.java
@@ -3,12 +3,16 @@ package engineers.core.items.batteries;
 import java.util.List;
 
 import engineers.core.items.ItemBase;
+import engineers.core.power.BatteryHandler;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.EnumHand;
+import net.minecraft.world.World;
 
 public class ItemBattery extends ItemBase {
 	public static enum BatteryTypes {
-		BASIC(4000, "Low"), ADVANCED(4000, "Advanced"), FUTURISTIC(4000, "Futuristic");
+		BASIC(4000, "Low"), ADVANCED(16000, "Advanced"), FUTURISTIC(64000, "Futuristic");
 		int max;
 		String name;
 
@@ -41,8 +45,14 @@ public class ItemBattery extends ItemBase {
 	@Override
 	public void addInformation(ItemStack stack, EntityPlayer playerIn, List<String> tooltip, boolean advanced) {
 		tooltip.add("Type: " + getType(stack).name);
-		tooltip.add("1337/" + getType(stack).max);
+		tooltip.add(BatteryHandler.getStoredPower(stack) + "/" + BatteryHandler.getCapacity(stack));
 		super.addInformation(stack, playerIn, tooltip, advanced);
+	}
+
+	@Override
+	public ActionResult<ItemStack> onItemRightClick(ItemStack stack, World world, EntityPlayer player, EnumHand hand) {
+		player.setHeldItem(hand, BatteryHandler.givePower(stack, 100L));
+		return super.onItemRightClick(stack, world, player, hand);
 	}
 
 }
---
+++ src/main/java/engineers/core/power/BatteryHandler.java
@@ -0,0 +1,43 @@
+package engineers.core.power;
+
+import engineers.core.items.batteries.ItemBattery;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+
+public class BatteryHandler {
+
+	public static long takePower(ItemStack stack, long power, boolean simulated) {
+		return 0;
+	}
+
+	public static long getStoredPower(ItemStack stack) {
+		if (isBattery(stack) && stack.getTagCompound() != null && stack.getTagCompound().hasKey("power"))
+			return stack.getTagCompound().getLong("power");
+		return 0;
+	}
+
+	public static long getCapacity(ItemStack stack) {
+		return ItemBattery.getType(stack).getMax();
+	}
+
+	public static long getUnfilled(ItemStack stack) {
+		return getCapacity(stack) - getStoredPower(stack);
+	}
+
+	public static ItemStack givePower(ItemStack stack, long power) {
+		if(stack.getTagCompound() == null)
+			stack.setTagCompound(new NBTTagCompound());
+		stack.getTagCompound().setLong("power", getStoredPower(stack) + power);
+		return stack;
+	}
+
+	public static boolean isBattery(ItemStack stack) {
+		return stack.getItem() instanceof ItemBattery;
+	}
+
+	public static ItemBattery getItemBattery(ItemStack stack) {
+		if (isBattery(stack))
+			return (ItemBattery) stack.getItem();
+		return null;
+	}
+}
